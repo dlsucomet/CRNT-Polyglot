b2a9ca8ca6d1289930d37bb29366fb17c5a95381
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-06-17 16:24:13 +0800

    Reimplemented CoNtRol file generation

---
+++ src/formats/control.ts
@@ -0,0 +1,12 @@
+import {ReactionNetwork, Reaction} from '../reaction';
+
+export function generateFile(rn: ReactionNetwork): string {
+  return rn.map(convertReaction).join("\n");
+}
+
+function convertReaction(r: Reaction): string {
+  let left = r.reactant.length ? r.reactant.join(" + ") : "0";
+  let right = r.product.length ? r.product.join(" + ") : "0";
+  var arrow = r.reversible ? "<-->" : "-->";
+  return left + " " + arrow + " " + right;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ReactionNetwork, Reaction, Complex, Term, rNetworkToString, rNetworkFromString } from './reaction';
 
+import * as ControlFormat from './formats/control';
+
 let R: ReactionNetwork = [
   new Reaction([new Term(1, "X1"), new Term(1, "X2")], [new Term(2, "X2"), new Term(1, "X3")]),
   new Reaction([new Term(1, "X4")], [], true),
 ];
 
+console.log("Reaction:");
 console.log(rNetworkToString(R));
-console.log(rNetworkToString(rNetworkFromString(rNetworkToString(R))));
-console.log(rNetworkToString(rNetworkFromString(rNetworkToString(R))) === rNetworkToString(R));
+
+console.log("Control:");
+console.log(ControlFormat.generateFile(R));
