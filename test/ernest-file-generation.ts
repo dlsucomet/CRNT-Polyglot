f468e090d28bd44c9bdaaf4a886d437cf56c587d
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-06-25 20:17:41 +0800

    Reimplemented ERNEST file generation

--- test/ernest-file-generation.ts
+++ test/ernest-file-generation.ts
@@ -21,7 +21,7 @@ export class Test implements TestFile {
       "",
       "model.id = 'Sample_Model';",
       "model.name = 'Sample Model';",
-      "model.species = struct('id', {'A', 'A1', 'C4', 'NC', 'S', 'S7' 'X1', 'X2', 'X3', 'X5'});",
+      "model.species = struct('id', {'A', 'A1', 'C4', 'NC', 'S', 'S7', 'X1', 'X2', 'X3', 'X5'});",
       "model.reaction(1) = struct('id', 'X1<->7X2', 'reactant', struct('species', 'X1', 'stoichiometry', 1), 'product', struct('species', 'X2', 'stoichiometry', 7), 'reversible', true);",
       "model.reaction(2) = struct('id', 'C4->2X3+X2', 'reactant', struct('species', 'C4', 'stoichiometry', 1), 'product', struct('species', {'X3', 'X2'}, 'stoichiometry', {2, 1}), 'reversible', false);",
       "model.reaction(3) = struct('id', 'A+NC+7S->A1+S7', 'reactant', struct('species', {'A', 'NC', 'S'}, 'stoichiometry', {1, 1, 7}), 'product', struct('species', {'A1', 'S7'}, 'stoichiometry', {1, 1}), 'reversible', false);",
