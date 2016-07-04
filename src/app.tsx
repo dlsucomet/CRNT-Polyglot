9ed33089087397b91b613f56c9ddee0b6dfc8e66
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-07-05 06:26:48 +0800

    Reintroduced the add independent reactions form

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,36 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import ReactionNetworkModel from './component-models/reaction-network-model';
-import ReactionNetworkTable from './components/reaction-network-table';
+import {observable, computed} from 'mobx';
+import {observer} from 'mobx-react';
 
-let rn = new ReactionNetworkModel();
-ReactDOM.render(<ReactionNetworkTable reactionNetwork={rn} />, document.getElementById('root'));
+import ReactionNetworkModel from './component-models/reaction-network-model';
+import FormModel from './component-models/add-form-model';
+
+import ReactionNetworkTable from './components/reaction-network-table';
+import AddIndependentReactionsForm from './components/add-independent-reactions-form';
+
+class AppModel {
+  @observable reactionNetwork: ReactionNetworkModel = new ReactionNetworkModel();
+  @observable showForm: boolean = false;
+  @observable formModel: FormModel = new FormModel(this.reactionNetwork);
+  @computed get generatedFiles() {
+    return null;
+  }
+}
+
+@observer
+class App extends React.Component<{model: AppModel}, {}> {
+  render() {
+    let m = this.props.model;
+    return (
+      <div>
+        <ReactionNetworkTable reactionNetwork={m.reactionNetwork} />
+        <AddIndependentReactionsForm model={m.formModel} />
+      </div>
+    );
+  }
+}
+
+let app = new AppModel();
+ReactDOM.render(<App model={app} />, document.getElementById('root'));
