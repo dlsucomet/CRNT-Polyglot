3f25901000ff7b23d728e62907799ad982b9fbb3
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-07-04 22:38:49 +0800

    Fixed the add form to update its model

--- src/components/add-independent-reactions-form.tsx
+++ src/components/add-independent-reactions-form.tsx
@@ -12,18 +12,34 @@ export default class AddIndependentReactionsForm extends React.Component<{model:
       <div>
         <h2>Add Independent Reactions</h2>
         <label>Species</label>
-        <input value={m.variable} />
+        <input value={m.variable} onChange={this.updateVariable} />
         <label>Start Index</label>
-        <input type="number" value={m.startIndex} />
+        <input type="number" value={m.startIndex} onChange={this.updateStartIndex} />
         <label>End Index</label>
-        <input type="number" value={m.endIndex} />
+        <input type="number" value={m.endIndex} onChange={this.updateEndIndex} />
         <button onClick={this.add}>Add</button>
       </div>
     );
   }
 
-  // TODO: handle change events!
+  updateVariable = (e: Event) => {
+    let input = e.target as HTMLInputElement;
+    let m = this.props.model;
+    m.variable = input.value;
+  }
 
+  updateStartIndex = (e: Event) => {
+    let input = e.target as HTMLInputElement;
+    let m = this.props.model;
+    m.startIndex = parseInt(input.value);
+  }
+
+  updateEndIndex = (e: Event) => {
+    let input = e.target as HTMLInputElement;
+    let m = this.props.model;
+    m.endIndex = parseInt(input.value);
+  }
+  
   add = () => {
     let m = this.props.model;
     m.reactionNetwork.addIndependentReactions(m.variable, m.startIndex, m.endIndex);
